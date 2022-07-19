caa38e56303c9d9a33ac2bdd92b85b7ad100c4e9
AbdulazizAlkhars <alkharswork@gmail.com>
2022-07-19 19:13:48 +0300

    Done

--- games/urls.py
+++ games/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from pokemon import views
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # path("pokemons/", views.get_pokemon),
+    path("pokemons/<int:pokemon_id>/", views.get_pokemon),
+    path("pokemons/", views.get_pokemon_list),
 ]
--- pokemon/views.py
+++ pokemon/views.py
@@ -1,3 +1,16 @@
 from django.http import HttpResponse
-
 from .models import Pokemon
+
+
+def get_pokemon(request, pokemon_id):
+    pokemon = Pokemon.objects.get(id=pokemon_id)
+    pokemon_data_list = "\n".join(
+        f"{pokemon.id} - {pokemon.name}")
+    return HttpResponse(f"<b>{pokemon_data_list}</b>")
+
+
+def get_pokemon_list(request):
+    pokemon_list = Pokemon.objects.all()
+    pokemon_list = "\n".join(
+        f"<p>{pokemon.id} - {pokemon.name} - {pokemon.type} - {pokemon.hp} - {pokemon.active} <p>" for pokemon in pokemon_list)
+    return HttpResponse(f"<b>{pokemon_list}</b>")
